b088dca35e5230918314e1ca759950099a2ac6cb
Gripsztal <mcjkubasiak@student.agh.edu.pl>
2025-04-15 18:29:32 +0200

    Add files via upload

---
+++ generatory_paczka/zad5.py
@@ -0,0 +1,6 @@
+# Zad. 5
+# Użyj `itertools.count`, aby stworzyć nieskończony licznik zaczynający się od 10,
+# który zwiększa się co 5.
+# Następnie przerwij działanie po znalezieniu 5 liczb (podzielnych przez 3).
+
+import itertools
